7ad6801d7afc17d112503353cde52cb0874b98cc
WillianCastelano <134349319+WillianCastelano@users.noreply.github.com>
2025-08-06 10:42:24 -0300

    Criacao de IllegalArgumentException

--- src/main/java/com/example/projeto01/exceptions/ApplicationExceptionHandler.java
+++ src/main/java/com/example/projeto01/exceptions/ApplicationExceptionHandler.java
@@ -10,10 +10,22 @@ import org.springframework.web.reactive.result.method.annotation.ResponseEntityE
 @Slf4j//annotation do Lombok para gerar automaticamente um logger para a classe onde ela é usada.
 @ControllerAdvice
   public class ApplicationExceptionHandler extends  ResponseEntityExceptionHandler {
-    @ExceptionHandler(Exception.class)
+
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException ex) {
+        log.info("Caiu no IllegalArgumentException");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro de validação: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> handleRuntime(RuntimeException ex) {
         log.info("Caiu no RunTimeException");
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro interno: " + ex.getMessage());
 
     }
+
+
+
+
 }
